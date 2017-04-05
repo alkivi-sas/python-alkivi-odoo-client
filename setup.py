1277042624126c074f6c6162925eb5685673bf6f
Anthony Martinet <anthony@alkivi.fr>
2017-04-05 12:49:49 +0200

    fix(build): when no pypandoc

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ try:
     import pypandoc
     long_description = pypandoc.convert(readme, 'rst')
 except(IOError, ImportError):
-    long_description = open(readme).read()
+    long_description = ""
 
 setup(
     name='alkivi-odoo-client',
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.2',
+    version='1.0.3',
 
     description='Odoo python client used at Alkivi',
     long_description=long_description,
